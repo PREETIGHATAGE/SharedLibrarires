a2fe42e84693233d843b5b6c2e4d1e671651d863
PREETIGHATAGE <164339733+PREETIGHATAGE@users.noreply.github.com>
2024-04-12 21:05:03 +0530

    Update cicd.groovy

--- cicd.groovy
+++ cicd.groovy
@@ -5,7 +5,7 @@ git "${repo}"
 {
 sh "mvn package"
 }
-def newDeploy(http://172.31.86.203:8080, testapp)
+def newDeploy(ip, appname)
 {
 deploy adapters: [tomcat9(credentialsId: '8a3325e9-d38f-4e77-be7d-f009417b0ffb', path: '', url: "${http://172.31.86.203:8080}")], contextPath: "${testapp}", war: '**/*.war'
 }
