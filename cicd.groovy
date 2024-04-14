91deca40f0fb9de795c0a2526a6e0065de10c2c8
PREETIGHATAGE <164339733+PREETIGHATAGE@users.noreply.github.com>
2024-04-14 12:12:29 +0530

    Update cicd.groovy

--- cicd.groovy
+++ cicd.groovy
@@ -2,18 +2,5 @@ def newGit(repo)
 {
 git "${repo}"
 }
-def newMaven()
-{
-sh "mvn package"
-}
-def newDeploy(ip, appname)
-{
-deploy adapters: [tomcat9(credentialsId: '8a3325e9-d38f-4e77-be7d-f009417b0ffb', path: '', url: "${http://172.31.86.203:8080}")], contextPath: "${testapp}", war: '**/*.war'
-}
-def newTest(newpipeline)
-{
-echo "${newpipeline}"
-sh "java -jar /home/ubuntu/.jenkins/workspace/${newpipeline}/testing.jar"
-}
 
 
